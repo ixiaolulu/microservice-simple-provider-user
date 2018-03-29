ff28ee6c3cd044ff2dc813b0f35c6d17b042a921
xianlu.ding <xianlu.ding@guanaitong.com>
2018-03-29 10:15:30 +0800

    spring-cloud学习

--- src/main/java/com/lulu/user/ProviderUserApplication.java
+++ src/main/java/com/lulu/user/ProviderUserApplication.java
@@ -2,6 +2,7 @@ package com.lulu.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2018/3/27 17:45
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderUserApplication.class, args);
